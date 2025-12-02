2713d2170add330213c2dd61e266ff5b8fb50665
einfachstiffi-boop <einfachstiffi@gmail.com>
2025-12-02 19:49:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,7 @@ start_date = st.date_input("Which date do you want to start looking for?", value
 option = st.selectbox(
     "What country would you want to search in?",
     ("DE", "AU", "CH"),
-)
-
-st.write("You selected:", option) #here we can select the country which will be implemented into the api
+) #here we can select the country which will be implemented into the api
 
 search = st.button("Search") #thats just the button to start the process/search
 
