8225bc24c7d755de2a039a62eab53cc35e0dc5c2
oscarpodewils-dev <oscar.podewils@gmail.com>
2025-12-11 14:11:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ BIN_KEYWORDS = {
 
 genre_id = "KnvZfZ7vAvF"
 
-def concerts_API(city: str, start: date, genre_id ):
+def concerts_API(city: str, start: date, predicted_bin ):
 
     # TEMP: hard-code POP to test Ticketmaster
      # POP genreId
@@ -182,7 +182,7 @@ def concerts_API(city: str, start: date, genre_id ):
 #Search
 if search and city.strip() != "": #here we validate that the city insert field is not empty else it will show the error message at the end of this code
     with st.spinner("Searching for concerts..."):
-        st.session_state['concerts'] = concerts_API(city.strip(), start_date, genre_id)
+        st.session_state['concerts'] = concerts_API(city.strip(), start_date, predicted_bin)
  #sort_concerts_by_genre_by_ai(options)   #here we convert the information the user gave us like the city and starting date
 
 concerts = st.session_state.get('concerts', pd.DataFrame())
